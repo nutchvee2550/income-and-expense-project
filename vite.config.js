d7225a51575a8a621eb1754f584186f7df7a8e0d
nutchvee2550 <nutchvee2550@gmail.com>
2026-09-03 22:41:57 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: "/income-and-expense-project/",   // ต้องตรงกับชื่อ repo
   plugins: [react()],
